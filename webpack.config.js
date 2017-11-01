068dd728c1f3db1659b8072375680f27f4e08a55
Owain <owain@Owains-MacBook-Pro.local>
2017-11-01 15:14:40 +0100

    updating the npm package and removing local running of the repo

--- webpack.config.js
+++ webpack.config.js
@@ -1,118 +1,40 @@
-const path = require('path')
 const webpack = require('webpack')
-
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-
-const devServer = {
-   contentBase: path.join(__dirname, '/public'),
-   port: 3000,
-   inline: true,
-   hot: true,
-   host: 'localhost',
-   stats: {
-      colors: true,
-      errors: true,
-      warnings: true,
-      modules: true,
-      chunks: true,
-      assets: true,
-      hash: true
-   },
-}
+const path = require('path')
 
 module.exports = env => {
-   const sourceMap = env.prod ? 'source-map' : 'eval'
-   
-   let plugins = []
-   
-   if (env.prod) {
-      plugins.push(
-         new webpack.optimize.UglifyJsPlugin({
-            sourceMap: true
-         }),
-         new webpack.DefinePlugin({
-            'process.env': {
-               'NODE_ENV': JSON.stringify('production')
-            }
-         })
-      )
-   }
-
-   if (env.dev) {
-      plugins.push(
-         new webpack.HotModuleReplacementPlugin(),
-         new HtmlWebpackPlugin({
-          inject: true,
-          template: path.join(__dirname, 'public', 'index.html')
-        })
-      )
-   }
-
-   const output = env.prod 
-      ? {
-         filename: 'bundle.js',
-         path: path.join(__dirname, 'dist'),
-         publicPath: '/'
-      } : {
-         path: path.join(__dirname, 'dist'),
-         pathinfo: true,
-         filename: 'static/js/bundle.js',
-         chunkFilename: 'static/js/[name].chunk.js',
-         publicPath: '/'
-      }
-
-   let config = {
-      entry: process.env.ENTRY,
-      output,
-      devtool: sourceMap,
+   return {
+      entry: './src/index.js',
+      output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'index.js',
+        libraryTarget: 'commonjs2'
+      },
       module: {
          rules: [
             {
-               test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
-               loader: require.resolve('url-loader'),
-               options: {
-                 limit: 10000,
-                 name: 'static/media/[name].[hash:8].[ext]',
-               },
-            },
-            {
                test: /\.jsx?$/,
-               exclude: /node-modules/,
+               exclude: /(node_modules|bower_components|build)/,
                use: [{
                   loader: 'babel-loader',
+                  options: {
+                    presets: ['env']
+                  }
                }],
-               include: path.join(__dirname, '/'),
-            },
-            {
-               test: /\.(css|scss|sass)$/,
-               include: path.join(__dirname, '/'),
-               use: [
-                  'style-loader',
-                  {
-                     loader: 'css-loader',
-                     options: {
-                        sourceMap,
-                        importLoaders: 1,
-                        // modules: true // breaks highlight.js theme style import
-                     }
-                  },
-                  {
-                     loader: 'sass-loader',
-                     options: {
-                        sourceMap,
-                     }
-                  },
-                  'postcss-loader'
-               ]
+               include: path.resolve(__dirname, 'src'),
             },
          ]
       },
-      plugins
-   }
-
-   if (env.dev) {
-      config['devServer'] = devServer
-   }
-
-   return config
+      resolve: {
+        extensions: ['.js', '.jsx']
+      },
+      externals: {
+        'react': 'commonjs react', // This line is to use React dependency of the parent project
+        'react-dom': 'commonjs react-dom'
+      },
+      plugins: [
+        new webpack.DefinePlugin({
+          'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+        })
+      ]
+  }
 }
