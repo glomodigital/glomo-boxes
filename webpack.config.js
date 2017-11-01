95eb2f35adee1281780b4abb75fd8def20768e0f
Owain <owain@Owains-MacBook-Pro.local>
2017-11-01 20:22:17 +0100

    fixed entry point in package.json

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,6 @@ module.exports = () => {
       output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'index.js',
-        libraryName: 'glomo-boxes',
         libraryTarget: 'umd'
       },
       module: {
