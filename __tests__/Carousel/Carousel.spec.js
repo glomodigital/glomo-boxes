3296c96217eb7dad594f2b3cd5ac7a7fc6361426
Owain <owain@Owains-MacBook-Pro.local>
2017-11-21 10:37:29 +0100

    updated test for carousel to mount refs for testing

--- __tests__/Carousel/Carousel.spec.js
+++ __tests__/Carousel/Carousel.spec.js
@@ -1,18 +1,30 @@
 import React from 'react'
-import renderer from 'react-test-renderer'
+import { mount } from 'enzyme'
+import toJson from 'enzyme-to-json'
 
 import { Carousel } from '../../lib'
 
+import { configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+configure({ adapter: new Adapter() })
+
 test('renders without crashing', () => {
-   const component = renderer.create(
+   let carousel = (
       <Carousel height={330}>
-         <div style={{ width: '100%', height: '100%', backgroundColor: 'blue' }} />
+         <div
+            style={{ width: '100%', height: '100%', backgroundColor: 'blue' }}
+         />
          <div
             style={{ width: '100%', height: '100%', backgroundColor: 'green' }}
          />
-         <div style={{ width: '100%', height: '100%', backgroundColor: 'red' }} />
+         <div
+            style={{ width: '100%', height: '100%', backgroundColor: 'red' }}
+         />
       </Carousel>
    )
-   
-   expect(component).toMatchSnapshot()
+
+   const component = mount(carousel)
+
+   expect(toJson(component)).toMatchSnapshot()
 })
