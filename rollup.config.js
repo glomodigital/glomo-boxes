c0a8930a7aca5ea94f17d5e1be8b7e0602c0aa34
Owain <owain@Owains-MacBook-Pro.local>
2017-11-27 23:27:49 +0100

    working on showcase tiles

--- rollup.config.js
+++ rollup.config.js
@@ -23,7 +23,6 @@ import nested from 'postcss-nested'
 import postcssImport from 'postcss-import'
 import postcssUrl from 'postcss-url'
 
-
 // Convert CJS modules to ES6, so they can be included in a bundle
 import commonjs from 'rollup-plugin-commonjs'
 
@@ -33,14 +32,16 @@ const cssExportMap = {}
 const NODE_MODULES_EXCLUDE = 'node_modules/**'
 
 const output_dir = path.resolve(__dirname, 'build')
-const input_dir = isProd ? path.resolve(__dirname, 'lib', 'index.js') : path.resolve(__dirname, 'lib', 'app.js')
+const input_dir = isProd
+   ? path.resolve(__dirname, 'lib', 'index.js')
+   : path.resolve(__dirname, 'lib', 'app.js')
 
 import pkg from './package.json'
 const external = Object.keys(pkg.dependencies)
 
 let postCssPlugins = [
    postcssImport({
-      root: 'node_modules'
+      root: 'node_modules',
    }),
    postcssUrl({ url: 'rebase' }),
    cssnext({ warnForDuplicates: false }),
@@ -48,21 +49,22 @@ let postCssPlugins = [
    nested(),
    postcssModules({
       getJSON(id, exportTokens) {
-         cssExportMap[id] = exportTokens;
-      }
-   })
+         cssExportMap[id] = exportTokens
+      },
+   }),
 ]
 
 let rollupPlugins = [
-
    // Define environment variables in the library
    replace({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+      'process.env.NODE_ENV': JSON.stringify(
+         process.env.NODE_ENV || 'development'
+      ),
    }),
 
    resolve({
       customResolveOptions: {
-         moduleDirectory: 'node_modules'
+         moduleDirectory: 'node_modules',
       },
       jsnext: true,
       main: true,
@@ -74,30 +76,34 @@ let rollupPlugins = [
       sourceMap: 'inline',
       plugins: isProd ? [...postCssPlugins, cssnano()] : postCssPlugins,
       getExportNamed: false,
-      getExport (id) {
-         return cssExportMap[id];
-      }
+      getExport(id) {
+         return cssExportMap[id]
+      },
    }),
 
    commonjs({
       include: 'node_modules/**',
       namedExports: {
-         'node_modules/react/index.js': ['PropTypes', 'createElement', 'Component', 'PureComponent', 'cloneElement', 'Children'],
+         'node_modules/react/index.js': [
+            'PropTypes',
+            'createElement',
+            'Component',
+            'PureComponent',
+            'cloneElement',
+            'Children',
+         ],
          'node_modules/react-dom/index.js': ['findDOMNode'],
       },
    }),
 
    // Allow transpilation of future JS
-   babel({
-      exclude: NODE_MODULES_EXCLUDE,
-      plugins: ['external-helpers']
-   }),
+   babel(),
 
    // limit imports or url() in project
    url({ limit: 10 * 1024, emitFiles: true }),
 
    // show the output bundle size in the CLI
-   filesize()
+   filesize(),
 ]
 
 if (isProd) {
@@ -117,8 +123,8 @@ if (isProd) {
          externals: [
             // { type: 'css', file: 'main.css', inject: 'head' },
             { type: 'css', file: 'atom-one-dark.css', inject: 'head' },
-            { type: 'js', file: 'index.js', inject: 'body' }
-         ]
+            { type: 'js', file: 'index.js', inject: 'body' },
+         ],
       }),
 
       // devserver instantiation
@@ -139,10 +145,10 @@ export default {
       format: 'cjs',
       name: 'glomo-boxes',
       sourcemap: true,
-      sourcemapFile: output_dir
+      sourcemapFile: output_dir,
    },
    plugins: rollupPlugins,
 
    // external dependencies of the project
-   external: (isProd && external)
+   external: isProd && external,
 }
